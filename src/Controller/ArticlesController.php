a762376a523257fa4e5c19671143f29b0cab65e7
miyabe <miyabe@daishokagaku.com>
2019-11-21 16:16:53 +0900

    チュートリアル2

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -11,4 +11,28 @@ class ArticlesController extends AppController
         $articles = $this->Paginator->paginate($this->Articles->find());
         $this->set(compact('articles'));
     }
+    
+    public function view($slug = null)
+	{
+	    $article = $this->Articles->findBySlug($slug)->firstOrFail();
+	    $this->set(compact('article'));
+	}
+	
+	 public function add()
+    {
+        $article = $this->Articles->newEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+
+            // user_id の決め打ちは一時的なもので、あとで認証を構築する際に削除されます。
+            $article->user_id = 1;
+
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+        $this->set('article', $article);
+    }
 }
