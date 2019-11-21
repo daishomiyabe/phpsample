00be6a927259eef22115561be5d7a38f5948247e
miyabe <miyabe@daishokagaku.com>
2019-11-21 17:33:10 +0900

    T4

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,6 +3,8 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
+use Cake\Validation\Validator;
 
 class ArticlesTable extends Table
 {
@@ -10,4 +12,26 @@ class ArticlesTable extends Table
     {
         $this->addBehavior('Timestamp');
     }
+    
+    public function beforeSave($event, $entity, $options)
+	{
+	    if ($entity->isNew() && !$entity->slug) {
+	        $sluggedTitle = Text::slug($entity->title);
+	        // スラグをスキーマで定義されている最大長に調整
+	        $entity->slug = substr($sluggedTitle, 0, 191);
+	    }
+	}
+	
+	public function validationDefault(Validator $validator)
+	{
+	    $validator
+	        ->allowEmptyString('title', false)
+	        ->minLength('title', 10)
+	        ->maxLength('title', 255)
+
+	        ->allowEmptyString('body', false)
+	        ->minLength('body', 10);
+
+	    return $validator;
+	}
 }
