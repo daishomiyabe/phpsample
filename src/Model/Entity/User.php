bce9c45b504988167521e361be66e23d42ca7df1
miyabe <miyabe@daishokagaku.com>
2019-11-22 14:49:15 +0900

    ＰＷハッシュ化

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher; // この行を追加
 use Cake\ORM\Entity;
 
 /**
@@ -41,4 +42,16 @@ class User extends Entity
     protected $_hidden = [
         'password'
     ];
+    
+    // bake のコード
+
+    // このメソッドの追加
+    protected function _setPassword($value)
+    {
+        if (strlen($value)) {
+            $hasher = new DefaultPasswordHasher();
+
+            return $hasher->hash($value);
+        }
+    }
 }
