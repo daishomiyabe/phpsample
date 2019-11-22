7fec5a6e82236d51e87c349a67a96b1612602f66
miyabe <miyabe@daishokagaku.com>
2019-11-22 16:37:26 +0900

    おためし

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -83,16 +83,4 @@ class AppController extends Controller
     	
     	return false;
     } 
-    
-    public function login(){
-    	if($this->request->is('post')){
-    		$user = $this->Auth->identify();
-    		if($user){
-    			$this->Auth->setUser($user);
-    			return $this->redirect($this->Auth->redirectUrl());
-    		}
-    		$this->Flash->error('ユーザー名またはパスワードが不正です');
-    	}
-    }
-    		
 }
--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -12,6 +12,27 @@ use App\Controller\AppController;
  */
 class UsersController extends AppController
 {
+    public function login(){
+    	if($this->request->is('post')){
+    		$user = $this->Auth->identify();
+    		if($user){
+    			$this->Auth->setUser($user);
+    			return $this->redirect($this->Auth->redirectUrl());
+    		}
+    		$this->Flash->error('ユーザー名またはパスワードが不正です');
+    	}
+    }
+    
+    public function initialize(){
+	    parent::initialize();
+	    $this->Auth->allow(['logout']);
+    }
+    
+    public function logout(){
+    	$this->Flash->success('ログアウトしました');
+    	return $this->redirect($this->Auth->logout());
+    }
+    		
     /**
      * Index method
      *
